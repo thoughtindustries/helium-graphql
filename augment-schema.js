8c739417e4cb070a2a7a8dab888bbef8c114bfbe
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-06-24 18:39:53 +0000

    fix: fix formatting in description; fix name target

--- augment-schema.js
+++ augment-schema.js
@@ -6,35 +6,39 @@ const { types } = introspectionFile.__schema;
 const commonIdFields = ['bundle', 'course', 'learningPath', 'license', 'client'];
 const recordTimestampFields = ['createdAt', 'updatedAt'];
 
+const formatName = name => name.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })
+
 const handleTypesFields = (type, fields) => {
   const { name } = type;
   const definition = DEFINITION_MAP[name];
+  const formattedName = formatName(name);
 
   if (definition?.metadescription) {
     type.description = definition.metadescription;
   } 
 
   for (const field of fields) {
+    const formattedFieldName = formatName(field.name);
     let { description } = field;
 
     if (definition?.fields[field.name]) {
       description = definition.fields[field.name];
     } else if (field?.type?.name === 'ID' || field.name.endsWith('id')) {
       if (field.name === 'id') {
-        description = `The ID of the ${name}`;
+        description = `The ID of the ${formattedName}`;
       } else if (field.name.endsWith('id')) {
         const descSubject = field.name.slice(0, -2);
         description = `The ID of the ${descSubject}`;
       } else {
-        description = `The ID of the ${field.type.name}`;
+        description = `The ID of the ${formattedFieldName}`;
       }
     } else if (commonIdFields.includes(field.name)) {
-      description = `The ID of the ${field.name}`;
+      description = `The ID of the ${formattedFieldName}`;
     } else if (recordTimestampFields.includes(field.name)) {
       const propVerb = field.name.slice(0, -2);
       description = `The time the field was ${propVerb}`;
     } else if (field.name === 'deleted') {
-      description = `Returns true when the ${name} is deleted.`;
+      description = `Returns true when the ${formattedFieldName} is deleted.`;
     }
 
     field.description = description;
@@ -58,7 +62,7 @@ const handleQueryOrMutation = field => {
         description = definition.args[arg.name];
       } else if (arg?.type?.name === 'ID' || arg.name.endsWith('Id')) {
         const descSubject = arg.name.endsWith('Id') ? arg.name.slice(0, -2) : arg.name;
-        description = `The ID of the ${descSubject}`;
+        description = `The ID of the ${formatName(descSubject)}`;
       } else if (arg.name === 'page') {
         description = `The page number to return within the collection.`;
       }
