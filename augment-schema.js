ac9570a3e32f719dc69287fbe26c1231cd20a29e
Robert Schillinger <robert.schillinger@thoughtindustries.com>
2022-07-05 10:23:18 -0500

    Add Description Map and Example Generator (#1)
    * feat: augment descriptions in schema
    
    * feat: add definition map
    
    * ci: ignore public directory
    
    * fix: fix formatting in description; fix name target
    
    * fix: trim whitespace; fix 'deleted' desc regression
    
    * fix: update schema; update workflow to run augment-schema script

---
+++ augment-schema.js
@@ -0,0 +1,99 @@
+const introspectionFile = require('./graphql.schema.json');
+const DEFINITION_MAP = require('./definition-map');
+const { writeFile } = require('fs/promises');
+const { types } = introspectionFile.__schema;
+
+const commonIdFields = ['bundle', 'course', 'learningPath', 'license', 'client'];
+const recordTimestampFields = ['createdAt', 'updatedAt'];
+
+const formatName = name => name.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase().trim(); })
+const withoutId = name => name.replace(/\s[Ii]d$/, '');
+
+const handleTypesFields = (type, fields) => {
+  const { name } = type;
+  const definition = DEFINITION_MAP[name];
+  const formattedName = formatName(name);
+
+  if (definition?.metadescription) {
+    type.description = definition.metadescription;
+  } 
+
+  for (const field of fields) {
+    const formattedFieldName = formatName(field.name);
+    let { description } = field;
+
+    if (definition?.fields[field.name]) {
+      description = definition.fields[field.name];
+    } else if (field?.type?.name === 'ID' || field.name.endsWith('id')) {
+      if (field.name === 'id') {
+        description = `The ID of the ${withoutId(formattedName)}`;
+      } else if (field.name.endsWith('id')) {
+        const descSubject = field.name.slice(0, -2);
+        description = `The ID of the ${descSubject}`;
+      } else {
+        description = `The ID of the ${withoutId(formattedFieldName)}`;
+      }
+    } else if (commonIdFields.includes(field.name)) {
+      description = `The ID of the ${formattedFieldName}`;
+    } else if (recordTimestampFields.includes(field.name)) {
+      const propVerb = field.name.slice(0, -2);
+      description = `The time the field was ${propVerb}`;
+    } else if (field.name === 'deleted') {
+      description = `Returns true when the ${formattedName} is deleted.`;
+    }
+
+    field.description = description;
+  }
+};
+
+const handleQueryOrMutation = field => {
+  const definition = DEFINITION_MAP[field.name];
+
+  if (definition?.metadescription) {
+    field.description = definition.metadescription;
+  }
+
+  if (field.args.length) {
+    const { args } = field;
+
+    for (const arg of args) {
+      let { description } = arg;
+
+      if (definition?.args[arg.name]) {
+        description = definition.args[arg.name];
+      } else if (arg?.type?.name === 'ID' || arg.name.endsWith('Id')) {
+        const descSubject = arg.name.endsWith('Id') ? arg.name.slice(0, -2) : arg.name;
+        description = `The ID of the ${formatName(descSubject)}`;
+      } else if (arg.name === 'page') {
+        description = `The page number to return within the collection.`;
+      }
+
+      arg.description = description;
+    }
+
+    field.args = args;
+  }
+};
+
+(async function() {
+  for (const type of types) {
+    const { kind, name, fields } = type;
+
+    if (kind === 'OBJECT') {
+      if (name === 'Query' || name === 'Mutation') {
+        fields.forEach(field => handleQueryOrMutation(field));
+      } else if (name !== 'Query' && name !== 'Mutation') {
+        // defined Object Types, e.g., AllocatedLearningPath, UserPurchases, etc.
+        handleTypesFields(type, fields);
+      }
+    }
+
+    type.fields = fields;
+  }
+
+  introspectionFile.__schema.types = types;
+
+  await writeFile('./graphql.schema.json', JSON.stringify(introspectionFile, null, 2));
+})()
+  .then(() => process.exit(0))
+  .catch(err => console.error('err', err));
---
+++ definition-map.js
@@ -0,0 +1,35 @@
+const TYPES = {
+    CatalogMeta: {
+        metadescription: "Contains metadata about a requested Catalog.",
+        fields: {
+            contentTypes: "Content Types requested in the Catalog."
+        }
+    }
+};
+
+const QUERIES = {
+    CompanyTranslations: {
+        metadescription: "Returns translations from the user's Thought Industries instance.",
+        args: {
+            namespace: "The language key for which translations should be returned."
+        }
+    }
+};
+
+const MUTATIONS = {
+    ArchiveUserCourse: {
+        metadescription: "Archives a user's access to a course.",
+        args: {
+            id: "The ID of the course to be archived."
+        }
+        
+    }
+}
+
+const DEFINITION_MAP = {
+    ...TYPES,
+    ...QUERIES,
+    ...MUTATIONS
+}
+
+module.exports = DEFINITION_MAP;
