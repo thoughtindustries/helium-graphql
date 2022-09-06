a65496966bd941e01fe5333301eeb15574e0359e
Lu Jiang <89472381+ljiang-ti@users.noreply.github.com>
2022-09-06 09:54:09 -0700

    feat: update doc (#2)
    * feat: update schema
    
    * feat: update doc
    
    * feat: add description to new quiz attempt queries and mutations
    
    * feat: update generated artifact
    
    * feat: update description
    
    * fix: Update definition-map.js

--- augment-schema.js
+++ augment-schema.js
@@ -26,18 +26,18 @@ const handleTypesFields = (type, fields) => {
       description = definition.fields[field.name];
     } else if (field?.type?.name === 'ID' || field.name.endsWith('id')) {
       if (field.name === 'id') {
-        description = `The ID of the ${withoutId(formattedName)}`;
+        description = `The ID of the ${withoutId(formattedName)}.`;
       } else if (field.name.endsWith('id')) {
         const descSubject = field.name.slice(0, -2);
-        description = `The ID of the ${descSubject}`;
+        description = `The ID of the ${descSubject}.`;
       } else {
-        description = `The ID of the ${withoutId(formattedFieldName)}`;
+        description = `The ID of the ${withoutId(formattedFieldName)}.`;
       }
     } else if (commonIdFields.includes(field.name)) {
-      description = `The ID of the ${formattedFieldName}`;
+      description = `The ID of the ${formattedFieldName}.`;
     } else if (recordTimestampFields.includes(field.name)) {
       const propVerb = field.name.slice(0, -2);
-      description = `The time the field was ${propVerb}`;
+      description = `The time the field was ${propVerb}.`;
     } else if (field.name === 'deleted') {
       description = `Returns true when the ${formattedName} is deleted.`;
     }
@@ -63,7 +63,7 @@ const handleQueryOrMutation = field => {
         description = definition.args[arg.name];
       } else if (arg?.type?.name === 'ID' || arg.name.endsWith('Id')) {
         const descSubject = arg.name.endsWith('Id') ? arg.name.slice(0, -2) : arg.name;
-        description = `The ID of the ${formatName(descSubject)}`;
+        description = `The ID of the ${formatName(descSubject)}.`;
       } else if (arg.name === 'page') {
         description = `The page number to return within the collection.`;
       }
